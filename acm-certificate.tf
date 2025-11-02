cd22662c199108c993888e9a0abed62ef498f715
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-02 15:09:18 +0200

    adding the acm2

--- acm-certificate.tf
+++ acm-certificate.tf
@@ -8,10 +8,10 @@ resource "aws_acm_certificate" "lzb-cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name    = tolist(aws_acm_certificate.lzb-cert.domain_validation_options[0].resource_record_name)
-  type    = tolist(aws_acm_certificate.lzb-cert.domain_validation_options[0].resource_record_type)
+  name    = tolist(aws_acm_certificate.lzb-cert.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.lzb-cert.domain_validation_options)[0].resource_record_type
   zone_id = var.zone_id
-  records = [tolist(aws_acm_certificate.lzb-cert.domain_validation_options[0].resource_record_value)]
+  records = [tolist(aws_acm_certificate.lzb-cert.domain_validation_options)[0].resource_record_value]
   ttl     = 60
 }
 
