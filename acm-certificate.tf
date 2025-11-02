ef925345cd70d0d834170112c017afe734396032
Lupfumo <lupfumo@gmail.com>
2025-11-02 23:22:18 +0200

    apply 4

--- acm-certificate.tf
+++ acm-certificate.tf
@@ -5,6 +5,28 @@ resource "aws_acm_certificate" "lzb-cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.lzb-certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "lzb-certificate" {
+  certificate_arn         = aws_acm_certificate.lzb-certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+/*
+resource "aws_route53_record" "cert_validation" {
   name    = tolist(aws_acm_certificate.lzb-cert.domain_validation_options)[0].resource_record_name
   type    = tolist(aws_acm_certificate.lzb-cert.domain_validation_options)[0].resource_record_type
   zone_id = var.zone_id
@@ -15,4 +37,4 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "lzb-certificate" {
   certificate_arn         = aws_acm_certificate.lzb-cert.arn
   validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
-}
+}*/
