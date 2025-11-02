55786dce8a9f370d7d94e9b73c3fc058165c83bb
Lupfumo <lupfumo@gmail.com>
2025-11-02 18:38:16 +0200

    http

--- aws-alb.tf
+++ aws-alb.tf
@@ -9,8 +9,8 @@ resource "aws_lb" "alb" {
 
 resource "aws_lb_target_group" "tg" {
   name     = "lzb-alb-targets"
-  port     = 443
-  protocol = "HTTPS"
+  port     = 80
+  protocol = "HTTP"
   target_type = "ip"
   deregistration_delay = "30"
   vpc_id      = module.vpc.vpc_id
