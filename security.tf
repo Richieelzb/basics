fd2fa9ff139f1d81f3a02b9e60524c229217e7ad
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-02 14:54:56 +0200

    adding the sg

--- security.tf
+++ security.tf
@@ -19,6 +19,15 @@ resource "aws_security_group" "public-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
